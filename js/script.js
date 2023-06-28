951c6ded4fbcae43dccb9ea33ecbb152df33b306
jennycherny <evchern.it@gmail.com>
2023-06-28 18:24:15 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const tabs = require('./modules/tabs'),
-          modal = require('./modules/modal'),
-          calc = require('./modules/calc'),
-          cards = require('./modules/cards'),
-          forms = require('./modules/forms'),
-          slider = require('./modules/slider'),
-          timer = require('./modules/timer');
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import { openModal } from './modules/modal';
 
-    tabs();
-    modal();
+document.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerID = setTimeout(() => openModal('.modal', modalTimerID), 50000);
+
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerID);
     calc();
     cards();
-    forms();
-    slider();
-    timer();
+    forms('form', modalTimerID);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        slide: '.offer__slide',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total', 
+        currentCount: '#current', 
+        wrapper: '.offer__slider-wrapper', 
+        field: '.offer__slider-inner'
+    });
+    timer('.timer', '2023-07-11');
 });
